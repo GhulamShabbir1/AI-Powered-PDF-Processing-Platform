d92eb491bd535662128bf4a7b7007267a8e8c0f9
walimajid644 <147191282+walimajid644@users.noreply.github.com>
2026-04-17 18:32:26 +0500

    forget password page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/landing/Home.vue'
+import ForgetPassword from '../pages/auth/ForgetPassword.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/forgetpassword',
+      name: 'ForgetPassword',
+      component: ForgetPassword,
     },
     // Add other routes as needed
   ],
